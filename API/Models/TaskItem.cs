e4ff6dfb6594a3ef93b5c147115e10896aa343ea
B Matheus <matheussantos1924@hotmail.com>
2026-05-03 14:07:43 -0300

    Comentando código e pagina de testes, arrumando conflitos

--- API/Models/TaskItem.cs
+++ API/Models/TaskItem.cs
@@ -8,5 +8,10 @@ public int Id { get; set; }
 public string? Titulo { get; set; }
 public string? Descricao { get; set; }
 public bool Concluida { get; set; }
-public DateTime CriadoEm { get; set; }
+public DateTime CriadoEm { get; set; } = DateTime.Now;
+
+public int UsuarioId { get; set; }
+public Usuario? Usuario { get; set; }
+public int CategoriaId { get; set; }
+public Categoria? Categoria { get; set; }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,33 +1,123 @@
 using API.Data;
 using API.Models;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-
-// FEITO ATÉ AGORA
-// - Estrutura inicial criada
-// - Models (Usuario, Categoria, TaskItem)  -- clases e tabelas
-// - AppDataContext configurado
-// - Banco SQLite criado
-// - Primeira Migration aplicada
-// - Conexão API ↔ Banco funcionando
-
-// PROXIMOS PASSOS
-// - fazer o CRUD das tabelas Usuario, Categorias, TaskItem
-// - testar endpoints da api
-// - ajustar relacionamentos se necessário
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDataContext>();
 var app = builder.Build();
 
-// POST: /api/tarefa/cadastrar
-app.MapPost("/api/tarefa/cadastrar", ([FromBody] TaskItem tarefa, [FromServices] AppDataContext ctx) =>
+// ---------- ÁREA USUÁRIO ----------
+
+// CADASTRAR
+// POST: api/usuarios/cadastrar
+app.MapPost("/api/usuarios/cadastrar", async (Usuario usuario, AppDataContext context) =>
 {
-    tarefa.CriadoEm = DateTime.Now;
-    ctx.TaskItem.Add(tarefa);
-    ctx.SaveChanges();
-    return Results.Created($"/api/tarefa/{tarefa.Id}", tarefa);
+    context.Usuario.Add(usuario);
+    await context.SaveChangesAsync();
+    return Results.Created($"/usuarios/{usuario.Id}", usuario);
+});
+
+// LISTAR TODOS OS USUÁRIOS
+// GET: api/usuarios/listar
+app.MapGet("/api/usuarios/listar", async (AppDataContext context) =>
+{
+    return Results.Ok(await context.Usuario.ToListAsync());
+});
+
+// Listar por ID
+// GET /api/usuarios/{id}
+app.MapGet("/api/usuarios/{id}", async (int id, AppDataContext context) =>
+{
+    var usuario = await context.Usuario.FindAsync(id);
+    return usuario is not null ? Results.Ok(usuario) : Results.NotFound("Usuário não encontrado.");
+});
+
+// Atualizar USUÁRIO POR ID
+// PUT: api/usuarios/{id}
+app.MapPut("/api/usuarios/atualizar/{id}", async ([FromRoute] int id, [FromBody] Usuario usuarioAtualizado,AppDataContext context) =>
+{
+    var usuario = await context.Usuario.FindAsync(id);
+
+    if (usuario == null)
+        return Results.NotFound("Usuário não encontrado.");
+
+    usuario.Nome = usuarioAtualizado.Nome;
+    usuario.Email = usuarioAtualizado.Email;
+
+    await context.SaveChangesAsync();
+
+    return Results.Ok(usuario);
+});
+
+// REMOVER USUÁRIO
+// DELETE: api/usuarios/remover/{id}
+app.MapDelete("/api/usuarios/remover/{id}", async (int id, AppDataContext context) =>
+{
+    var usuario = await context.Usuario.FindAsync(id);
+
+    if (usuario == null)
+        return Results.NotFound("Usuário não encontrado.");
+
+    context.Usuario.Remove(usuario);
+    await context.SaveChangesAsync();
+    return Results.Ok("Usuário removido com sucesso.");
+});
+
+
+// --------- ÁREA CATEGORIAS -----------
+
+// CADASTRAR CATEGORIA
+// POST: /api/categorias/cadastrar
+app.MapPost("/api/categorias/cadastrar", async (Categoria categoria, AppDataContext context) =>
+{
+    context.Categoria.Add(categoria);
+    await context.SaveChangesAsync();
+    return Results.Created($"/categorias/{categoria.Id}", categoria);
+});
+
+// LISTAR TODAS AS CATEGORIAS
+// GET: api/categorias/listar
+app.MapGet("api/categorias/listar", async (AppDataContext context) =>
+{
+    return Results.Ok(await context.Categoria.ToListAsync());
+});
+
+// LISTAR CATEGORIA POR ID
+// GET: api/categorias/{id}
+app.MapGet("/api/categorias/{id}", async (int id, AppDataContext context) =>
+{
+    var categoria = await context.Categoria.FindAsync(id);
+    return categoria is not null ? Results.Ok(categoria) : Results.NotFound("Categoria não encontrada.");
+});
+
+// ATUALIZAR CATEGORIA POR ID
+// PUT: /api/categorias/atualizar/{id}
+app.MapPut("/api/categorias/atualizar/{id}", async (int id, Categoria categoriaAtualizada, AppDataContext context) =>
+{
+    var categoria = await context.Categoria.FindAsync(id);
+
+    if (categoria == null)
+        return Results.NotFound("Categoria não encontrada.");
+
+    categoria.Nome = categoriaAtualizada.Nome;
+
+    await context.SaveChangesAsync();
+    return Results.Ok(categoria);
+});
+
+// REMOVER CATEGORIA
+// DELETE: /api/categorias/remover/{id}
+app.MapDelete("/api/categorias/remover/{id}", async (int id, AppDataContext context) =>
+{
+    var categoria = await context.Categoria.FindAsync(id);
+
+    if (categoria == null)
+        return Results.NotFound("Categoria não encontrada.");
+
+    context.Categoria.Remove(categoria);
+    await context.SaveChangesAsync();
+    return Results.Ok("Categoria removida com sucesso.");
 });
 
 app.Run();
-
